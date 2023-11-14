4fcb47c3709095c0abdc30d3a13058e2a075fb39
derReloader <62664722+derReloader@users.noreply.github.com>
2023-11-14 09:14:33 +0100

    Supervisor: - Delete unused classes

--- src/systems/supervisor/backend/src/main/java/dk/sdu/se23g6/arch/supervisor/model/OrderResponse.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.se23g6.arch.supervisor.model;
-
-
-public class OrderResponse {
-    private int robotId;
-    private int orderId;
-}
--- src/systems/supervisor/backend/src/main/java/dk/sdu/se23g6/arch/supervisor/mongodb/OrdersRepository.java
+++
@@ -1,3 +0,0 @@
-package dk.sdu.se23g6.arch.supervisor.mongodb;
-
-// public interface OrdersRepository extends MongoRepository<Order, String> { }
